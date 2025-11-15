f6a51f748d6faa011b1ea483f98600e7cebfab6e
Usha Gotame <ushagotame000@gmail.com>
2025-11-15 22:21:02 +0545

    fix: baseUrl

--- src/features/users/userAPI.ts
+++ src/features/users/userAPI.ts
@@ -1,6 +1,9 @@
  import type { User } from '../../types/user'
 import axios from 'axios'
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL
+
  export const fetchUsersFromAPI = async (): Promise<User[]> => {
- const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+ const res = await axios.get(`${BASE_URL}/users`)
  return res.data as User[]
  }
